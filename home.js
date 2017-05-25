512f374f0d7efb270234a6c74bfe58a2feb37f42
Rghebrihiwet <rghebrihiwet@spartaglobal.co>
2017-05-25 11:44:43 +0100

    score's working on click

--- home.js
+++ home.js
@@ -21,98 +21,121 @@ function changeColor (){
 	});
 }
 
-$('#item').on('click', function(){
-	totalScore ++
-	$('#item').hide();
-	$('#scoree').html(totalScore);
+$('.balloon').on('click', function() {
+	var id = '#' + $(this).attr('id');
+	console.log('id', id);
+	console.log('background-color', $(id).css('background-color'));
+	if($(id).css('background-color') === 'rgb(255, 0, 0)') {
+		totalScore --;
+		$(id).hide();
+		$('#scoree').html(totalScore);
+		console.log('this', $(this).css('background-color'));
+	} else {
+		totalScore++;
+		$(id).hide();
+		$('#scoree').html(totalScore);
+	}
 });
+	// else {
+	// 	$('#item').css('background-color', 'green')
+	// 	totalScore ++;
+	// 	$('#item').hide();
+	// 	$('#scoree').html(totalScore);
 
-$('#item2').on('click', function(){
-	totalScore ++
-    $('#item2').hide();
-    $('#scoree').html(totalScore);
- });
+// $('.balloon').on('click', function(){
+// 	$('.balloon').css('background-color', 'red')
+// 	totalScore --;
+// 	$('#item').hide();
+// 	$('#scoree').html(totalScore);
+// 	console.log("working")
+// });
 
-$('#item3').on('click', function(){
-	totalScore ++
-	$('#scoree').html(totalScore);
-	$('#item3').hide();
- });
+// $('#item2').on('click', function(){
+// 	totalScore ++
+//     $('#item2').hide();
+//     $('#scoree').html(totalScore);
+//  });
 
-$('#item4').on('click', function(){
-	totalScore ++
-	$('#scoree').html(totalScore);
-	$('#item4').hide();
- });
+// $('#item3').on('click', function(){
+// 	totalScore ++
+// 	$('#scoree').html(totalScore);
+// 	$('#item3').hide();
+//  });
 
-$('#item5').on('click', function(){
-	totalScore ++
-	$('#scoree').html(totalScore);
-    $('#item5').hide();
- });
+// $('#item4').on('click', function(){
+// 	totalScore ++
+// 	$('#scoree').html(totalScore);
+// 	$('#item4').hide();
+//  });
 
-$('#item6').on('click', function(){
-	totalScore ++
-	$('#scoree').html(totalScore);
-    $('#item6').hide();
-  });
+// $('#item5').on('click', function(){
+// 	totalScore ++
+// 	$('#scoree').html(totalScore);
+//     $('#item5').hide();
+//  });
 
-$('#item7').on('click', function(){
-	totalScore ++
-	$('#scoree').html(totalScore);
-	$('#item7').hide();
-  });
+// $('#item6').on('click', function(){
+// 	totalScore ++
+// 	$('#scoree').html(totalScore);
+//     $('#item6').hide();
+//   });
 
-$('#item8').on('click', function(){
-	totalScore ++
-	$('#scoree').html(totalScore);
-	$('#item8').hide();
-  });
+// $('#item7').on('click', function(){
+// 	totalScore ++
+// 	$('#scoree').html(totalScore);
+// 	$('#item7').hide();
+//   });
 
-$('#item9').on('click', function(){
-	totalScore ++
-	$('#scoree').html(totalScore);
-	$('#item9').hide();
-  });
+// $('#item8').on('click', function(){
+// 	totalScore ++
+// 	$('#scoree').html(totalScore);
+// 	$('#item8').hide();
+//   });
 
-$('#item10').on('click', function(){
-	totalScore ++
-	$('#scoree').html(totalScore);
-	$('#item10').hide();
-  });
+// $('#item9').on('click', function(){
+// 	totalScore ++
+// 	$('#scoree').html(totalScore);
+// 	$('#item9').hide();
+//   });
 
-$('#item11').on('click', function(){
-	totalScore ++
-	$('#scoree').html(totalScore);
-	$('#item11').hide();
-  });
+// $('#item10').on('click', function(){
+// 	totalScore ++
+// 	$('#scoree').html(totalScore);
+// 	$('#item10').hide();
+//   });
+
+// $('#item11').on('click', function(){
+// 	totalScore ++
+// 	$('#scoree').html(totalScore);
+// 	$('#item11').hide();
+//   });
 
 setInterval(function() {
  	// $(".item").animate({"colors":1000})
  	changeColor()
  	console.log ("working")
- 	 }, 1000);
+ 	 }, 3000);
 
-function addCounter(){
-        $('#item').click(function () {
-        var good = $(this)
-        console.log(person.balloon("good"))
+// function addCounter(){
+//         $('#item').click(function () {
+//         var good = $(this)
+//         console.log(person.balloon("good"))
 
-        // check to see if we've shot a bad person
-        if(person.class ('good')){    
-            counters--;
-            $('.score').html(counters);
-        }
-        else{
-            counters++
-            $('.score').html(counters);
-        }
+//         // check to see if we've shot a bad person
+//         if(person.class ('good')){    
+//             counters--;
+//             $('.score').html(counters);
+//         }
+//         else{
+//             counters++
+//             $('.score').html(counters);
+//         }
 
-        var id = '#' + $(this).attr('id');
-        changeImage(id);
-    })
-}
-        addCounter();
+//         var id = '#' + $(this).attr('id');
+//         changeImage(id);
+//     })
+// }
+//         addCounter();
 
 
 	
